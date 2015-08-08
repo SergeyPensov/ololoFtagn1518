eafa30ebff4623aa96a37b3bc4eb074a6cbc9364
sergey.pensov <SergeyPensov>
2015-08-08 23:10:39 +0300

    Fix problem with modules

---
+++ src/vis/Board.java
@@ -0,0 +1,49 @@
+package vis;
+
+/**
+ * Created by kirill on 8/8/2015.
+ */
+public class Board {
+
+    public static final float R = 1.0f/(float)Math.sqrt(3);
+    public static final float DY = R * 1.5f;
+
+    public int width;
+    public int height;
+    private int[] array;
+
+    public Board(int width, int height) {
+        this.width = width;
+        this.height = height;
+        array = new int[width*height];
+    }
+
+    public static FPoint getCoordsForIndexes(final Pivot index) {
+        float x = index.x;
+        if( 1 == (index.y & 1)) x += 0.5f;
+        float y = index.y * DY;
+        return new FPoint(x, y);
+    }
+
+    public static Pivot getIndexForCoordinates(final FPoint point) {
+        int j = Math.round(point.y / DY);
+        int i = Math.round( (1==(j&1)) ? point.x-0.5f : point.x);
+        return new Pivot(i,j);
+    }
+
+    public int readCell( int i, int j) {
+        final int index = getIndex(i, j);
+        return array[index];
+    }
+
+    public void setCell( int i, int j, int value) {
+        final int index = getIndex(i, j);
+        array[index] = value;
+    }
+
+    private int getIndex(int i, int j) {
+        if( i < 0 || i >= width || j < 0 || j >= height ) throw new IllegalArgumentException("indexes out of bounds");
+        return i + j*width;
+    }
+
+}
