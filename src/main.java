ff8422551855563993f3f813062877d9fec02479
Kirill <kirill.sidorchuk@teamdev.com>
2015-08-09 15:03:48 +0300

    automatic submit script generation

--- src/main.java
+++ src/main.java
@@ -79,16 +79,43 @@ public class main {
                 }
 
                 Solver solver = new Solver(problem, inputFileName);
-                final SolverResult[] results = solver.solveAll(new int[]{problem.sourceSeeds[0]});
+                final SolverResult[] results = solver.solveAll(new int[]{problem.sourceSeeds[1]});
 
                 Gson gson = new Gson();
                 final String resultJSON = gson.toJson(results);
-                saveFile( inputFileName + "_result.json", resultJSON);
+                saveFile(inputFileName + "_result.json", resultJSON);
+
+                final String submit = formatSubmitScript(resultJSON);
+                saveFile(inputFileName + "_result_submit.bat", submit);
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
+    }
+
+    private static String formatSubmitScript(String resultJSON) {
+        final String API_TOKEN = ":plrVW5TJZhcAW/2HmsWBJOnGd2vwab+5Ssq9Y1HVggk=";
+        final String TEAM_ID = "195";
+        final String OUTPUT = escape(resultJSON);
+
+        return String.format("c:/soft/curl -k --user %s -X POST -H \"Content-Type: application/json\" -d %s https://davar.icfpcontest.org/teams/%s/solutions",
+                API_TOKEN, OUTPUT, TEAM_ID);
+    }
+
+    private static String escape(final String resultJSON) {
+        StringBuilder sb = new StringBuilder(resultJSON.length());
+        for( int i=0; i<resultJSON.length(); ++i) {
+            char c = resultJSON.charAt(i);
+            if( c == '\"') {
+                sb.append("\\\"");
+            }
+            else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
     }
 }
